b94b0ea628a6671e3a27abe13a7d76ee7c97a1a7
Kacper Stokwisz <kacperstokwisz@gmail.com>
2020-07-15 00:53:29 +0200

    creating the bricks

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const ctx = canvas.getContext('2d');
 
 let score = 0;
 
+const brickRowCount = 9;
+const brickColumnCount = 5;
+
+
 // Create ball props
 const ball = {
     x: canvas.width / 2,
@@ -25,6 +29,40 @@ const paddle = {
     speed: 8,
     dx: 0
   };
+
+// Create brick props
+const brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true
+  };
+
+// Create bricks
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
+
+// Draw bricks on canvas
+function drawBricks() {
+    bricks.forEach(column => {
+      column.forEach(brick => {
+        ctx.beginPath();
+        ctx.rect(brick.x, brick.y, brick.w, brick.h);
+        ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent';
+        ctx.fill();
+        ctx.closePath();
+      });
+    });
+  }
 
 // Draw paddle on canvas
 function drawPaddle() {
@@ -49,6 +87,7 @@ function draw() {
     drawBall();
     drawPaddle();
     drawScore();
+    drawBricks();
 }
 
 // Draw score on canvas
