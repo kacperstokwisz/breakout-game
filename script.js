ec261acc71b49a49e0272430fe480efcaa852dac
Kacper Stokwisz <kacperstokwisz@gmail.com>
2020-07-15 01:24:53 +0200

    move paddle

--- script.js
+++ script.js
@@ -82,21 +82,66 @@ function drawBall() {
     ctx.closePath();
 }
 
-// Draw elements
-function draw() {
-    drawBall();
-    drawPaddle();
-    drawScore();
-    drawBricks();
-}
-
 // Draw score on canvas
 function drawScore() {
     ctx.font = '20px Arial';
     ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
 }
 
-draw();
+// Move paddle on canvas
+function movePaddle() {
+    paddle.x += paddle.dx;
+    // Wall detection
+    if(paddle.x + paddle.w > canvas.width) {
+        paddle.x = canvas.width - paddle.w;
+    }
+
+    if(paddle.x < 0) {
+        paddle.x = 0;
+    }
+}
+
+// Draw elements
+function draw() {
+    // clear canvas 
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle();
+    drawScore();
+    drawBricks();
+}
+
+// Update canvas drawing and animation
+function update() {
+    movePaddle();
+    // Draw elements
+    draw();
+
+    requestAnimationFrame(update);
+}
+
+update();
+
+// Keydown event
+function keyDown(e) {
+    if(e.key === 'Right' || e.key === 'ArrowRight') {
+        paddle.dx = paddle.speed;
+    } else if(e.key === 'Left' || e.key === 'ArrowLeft') {
+        paddle.dx = -paddle.speed;
+    }
+}
+
+// Keyup event
+function keyUp(e) {
+    if(e.key === 'Right' || e.key === 'ArrowRight' || e.key === 'Left' || e.key === 'ArrowLeft') {
+        paddle.dx = 0;
+    }
+}
+
+// Keyboard event handlers
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
+
 // Rules and close event handlers
 rulesBtn.addEventListener('click', () => rules.classList.add('show'));
 closeBtn.addEventListener('click', () => rules.classList.remove('show'));
